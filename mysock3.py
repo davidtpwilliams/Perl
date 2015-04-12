cb73822030afbdeba60ec6f8e6d72dc138398467
David Williams <blakey249@gmail.com>
2015-04-12 15:54:41 +1000

    Port scanner

---
+++ mysock3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from socket import * 
+
+if __name__ == '__main__':
+    target = raw_input('Enter host to scan: ')
+    targetIP = gethostbyname(target)
+    print 'Starting scan on host ', targetIP
+
+    #scan reserved ports
+    for i in range(22):
+        s = socket(AF_INET, SOCK_STREAM)
+
+        result = s.connect_ex((targetIP, i))
+
+        if(result == 0) :
+            print 'Port %d: OPEN' % (i,)
+        else:
+            print 'Port %d: CLOSED' % (i,)
+        s.close()
